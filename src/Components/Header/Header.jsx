9440cc340bf0830d113bb25d1828724c468c5093
hhasona <hhasona@gmail.com>
2021-08-12 13:52:00 +0300

    aaaa

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,21 +1,10 @@
 import React from "react";
-import MenuIcon from "@material-ui/icons/Menu";
 import "./style.css";
 import SideNavBar from "../SideNavBar/SideNavBar";
 function Header(props) {
   return (
     <div>
       {<SideNavBar sideNavBarOpen={props.sideNavBarOpen} />}
-      <header className="header">
-        <button
-          className="menuBtn"
-          onClick={() => props.setSideNavBarOpen(!props.sideNavBarOpen)}
-        >
-          {/* {" "} */}
-          <MenuIcon style={{ marginLeft: "20px", zIndex: "11" }} />
-        </button>
-        <p>Home</p>
-      </header>
     </div>
   );
 }
--- src/Components/UserInfo/UserInfo.jsx
+++ src/Components/UserInfo/UserInfo.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import "./style.css";
-const UserInfo = () => {
+
+const UserInfo = ({ sideNavBarOpen }) => {
   return (
     <div className="mainDivUserInfo">
-      <AccountCircleIcon style={{ margin: "auto" ,width: "100%", height: "200px"}} className="stam" />
+      <AccountCircleIcon
+        style={{ margin: "auto", width: "100%", height: "200px" }}
+        className="stam"
+      />
       {/* take user name from auth || cookies ??? */}
       <p>User Name</p>
     </div>
